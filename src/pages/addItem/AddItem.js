bbcdece76d0af799413cf42e6de7a7f96149483b
swdrana <tctr2s@gmail.com>
2022-09-14 21:05:24 +0600

    add item design done

--- src/pages/addItem/AddItem.js
+++ src/pages/addItem/AddItem.js
@@ -8,99 +8,74 @@ const AddItem = () => {
         Please fill this form carefully
       </p>
       <section className="p-6 dark:bg-gray-800 dark:text-gray-50">
-        <form className="container flex flex-col lg:flex-row mx-auto space-x-0 lg:space-x-5 ng-untouched ng-pristine ng-valid">
-          <fieldset className="p-6 lg:w-1/2 w-full border border-dashed rounded-md shadow-md dark:bg-gray-900">
-            <div className="col-span-full">
-              <input
-                type="file"
-                placeholder=""
-                className="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-indigo-400 border border-gray-700 dark:text-gray-900"
-              />
-            </div>
-            <div className="max-w-xs mx-auto rounded-md shadow-md dark:bg-gray-900 dark:text-gray-100">
-              <img
-                src="https://cdn.dribbble.com/users/731646/screenshots/16592987/add_product_page_4x.png"
-                alt=""
-                className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500"
-              />
-            </div>
-          </fieldset>
-          <fieldset className="p-6 lg:w-1/2 w-full border border-dashed rounded-md shadow-md dark:bg-gray-900">
-            <div className="col-span-full">
-              <input
-                type="file"
-                placeholder=""
-                className="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-indigo-400 border border-gray-700 dark:text-gray-900"
-              />
-            </div>
-            <div className="max-w-xs mx-auto rounded-md shadow-md dark:bg-gray-900 dark:text-gray-100">
-              <img
-                src="https://cdn.dribbble.com/users/731646/screenshots/16592987/add_product_page_4x.png"
-                alt=""
-                className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500"
-              />
-            </div>
-          </fieldset>
-        </form>
-
-        <fieldset className=" p-6 rounded-md shadow-sm dark:bg-gray-900">
-          <div className="space-y-2 col-span-full lg:col-span-1">
-            <p className="font-medium">Profile</p>
-            <p className="text-xs">Adipisci fuga autem eum!</p>
-          </div>
-          <div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
-            <div className="col-span-full sm:col-span-3">
-              <label for="username" className="text-sm">
-                Username
-              </label>
-              <input
-                id="username"
-                type="text"
-                placeholder="Username"
-                className="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-indigo-400 dark:border-gray-700 dark:text-gray-900"
-              />
-            </div>
-            <div className="col-span-full sm:col-span-3">
-              <label for="website" className="text-sm">
-                Website
-              </label>
-              <input
-                id="website"
-                type="text"
-                placeholder="https://"
-                className="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-indigo-400 dark:border-gray-700 dark:text-gray-900"
-              />
-            </div>
-            <div className="col-span-full">
-              <label for="bio" className="text-sm">
-                Bio
-              </label>
-              <textarea
-                id="bio"
-                placeholder=""
-                className="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-indigo-400 dark:border-gray-700 dark:text-gray-900"
-              ></textarea>
-            </div>
-            <div className="col-span-full">
-              <label for="bio" className="text-sm">
-                Photo
-              </label>
-              <div className="flex items-center space-x-2">
-                <img
-                  src="https://source.unsplash.com/30x30/?random"
-                  alt=""
-                  className="w-10 h-10 rounded-full dark:bg-gray-500 dark:bg-gray-700"
+        <form className="container mx-auto space-x-0 ng-untouched ng-pristine ng-valid">
+          <div className="flex flex-col lg:flex-row  lg:space-x-10 space-y-10 lg:space-y-0">
+            <fieldset className="p-6 lg:w-1/2 w-full border border-dashed rounded-md shadow-md dark:bg-gray-900">
+              <div className="col-span-full">
+                <input
+                  type="file"
+                  placeholder=""
+                  className="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-indigo-400 border border-gray-400 dark:text-gray-900"
                 />
-                <button
-                  type="button"
-                  className="px-4 py-2 border rounded-md dark:border-gray-100"
-                >
-                  Change
-                </button>
               </div>
-            </div>
+              <div className="max-w-xs mx-auto rounded-md shadow-md dark:bg-gray-900 dark:text-gray-100">
+                <img
+                  src="https://cdn.dribbble.com/users/731646/screenshots/16592987/add_product_page_4x.png"
+                  alt=""
+                  className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500"
+                />
+              </div>
+            </fieldset>
+            <fieldset className="p-6 lg:w-1/2 w-full border border-dashed rounded-md shadow-md dark:bg-gray-900 space-y-5">
+              <div className="col-span-full">
+                <input
+                  id="product-name"
+                  type="text"
+                  placeholder="Product Name"
+                  className="w-full p-1 rounded-md focus:ring focus:ring-opacity-75 focus:ring-indigo-400 border border-gray-400 dark:text-gray-500"
+                />
+              </div>
+              <div className="col-span-full flex space-x-5">
+                <input
+                  id="quantity"
+                  type="number"
+                  placeholder="Quantity"
+                  className="w-full p-1 rounded-md focus:ring focus:ring-opacity-75 focus:ring-indigo-400 border border-gray-400 dark:text-gray-900"
+                />
+                <input
+                  id="price"
+                  type="number"
+                  placeholder="Price"
+                  className="w-full p-1 rounded-md focus:ring focus:ring-opacity-75 focus:ring-indigo-400 border border-gray-400 dark:text-gray-900"
+                />
+              </div>
+              <div className="col-span-full">
+                <input
+                  id="supplier"
+                  type="text"
+                  placeholder="Supplier Name"
+                  className="w-full p-1 rounded-md focus:ring focus:ring-opacity-75 focus:ring-indigo-400 border border-gray-400 dark:text-gray-500"
+                />
+              </div>
+              <div className="col-span-full">
+                <textarea
+                  id="description"
+                  type="text"
+                  placeholder="Description"
+                  rows="6"
+                  className="w-full p-1 rounded-md focus:ring focus:ring-opacity-75 focus:ring-indigo-400 border border-gray-400 dark:text-gray-500"
+                ></textarea>
+              </div>
+            </fieldset>
           </div>
-        </fieldset>
+          <div className=" mx-auto w-full flex justify-center mt-10">
+            <input
+              type="submit"
+              value="Add"
+              className=" btn btn-success w-52"
+            />
+          </div>
+        </form>
       </section>
     </div>
   );
