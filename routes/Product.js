a88b158a75be3e72e57b5a7fb5d0f4fca285c6e7
swdrana <tctr2s@gmail.com>
2022-09-16 18:00:31 +0600

    find a product by id

--- routes/Product.js
+++ routes/Product.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ldd63ua.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -9,13 +9,10 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-
-
 /*------------------------------------------*\
                 User API Start
 \*------------------------------------------*/
 const productCollection = client.db("shoe-inventory").collection("product");
-
 
 router.get("/", async (req, res) => {
   const query = {};
@@ -24,11 +21,20 @@ router.get("/", async (req, res) => {
   res.send(allProducts);
 });
 
+// load single item using _id
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: ObjectId(id) };
+  const product = await productCollection.findOne(query);
+//   console.log(product);
+  res.send(product);
+});
+
 router.post("/", async (req, res) => {
-    const product = req.body;
-    console.log(product);
-    const result = await productCollection.insertOne(product);
-    res.send(result);
+  const product = req.body;
+  console.log(product);
+  const result = await productCollection.insertOne(product);
+  res.send(result);
 });
 
 module.exports = router;
